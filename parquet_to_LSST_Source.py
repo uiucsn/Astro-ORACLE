985af0c72f93c30f1b4a0eb81f68a8eef2c54c1c
ved <vedshah.email@gmail.com>
2024-02-07 15:11:50 -0600

    Initial stubbed code for generating augmented (windowed) tensors

--- parquet_to_LSST_Source.py
+++ parquet_to_LSST_Source.py
@@ -1,21 +1,40 @@
-import numpy as np
-import pandas as pd
 import polars as pl
-import pyarrow as pa
+from pathlib import Path
 
 from LSST_Source import LSST_Source
+from argparse import ArgumentParser
 
-def get_LSST_objects(parquet_file_path):
+def parse_args(argv=None):
+    parser = ArgumentParser(
+        prog='Parquet2LSST_Source',
+        description='Convert parquet files based on SNANA FITS to LSST_Source object tensors',
+    )
+    parser.add_argument('input', type=Path, 
+                        help='Path to folder containing parquet files')
+    # parser.add_argument('output', type=Path,
+    #                     help='Output tensors from the feature extraction of LSST_Source objects')
+
+    return parser.parse_args(argv)
+
+def main(argv=None):
+
+    args = parse_args(argv)
+
+    parquet_file_path = args.input
+    object_class = str(parquet_file_path).split('.')[0].split('/')[1]
 
     df = pl.read_parquet(parquet_file_path)
-    row = df[0]
+    print(f'Starting data augmentation for {parquet_file_path} Number of objects: {df.shape[0]} Feature length: {df.shape[1]}')
 
-    temp = LSST_Source(row, 'KN_B19')
-    temp.plot_lightcurve()
-    print(temp)
-    
+    for i in range(df.shape[0]):
+
+        row = df[i]
+        temp = LSST_Source(row, object_class)
+        l = temp.get_augmented_sources()
+
+    print(f"Completed conversion: {parquet_file_path}")
+        
 
 if __name__=='__main__':
 
-    parquet_file_path = 'data/data/elasticc2_train/parquet/KN_B19.parquet'
-    get_LSST_objects(parquet_file_path)
+    main()
