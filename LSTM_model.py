05545bf2d4f3a97388e211db83f7c2a10a2b8fc8
ved <vedshah.email@gmail.com>
2024-02-16 16:35:53 -0600

    Skeleton LSTM model for simultaneous processing of time series and static data

---
+++ LSTM_model.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+
+class LSTMClassifier(nn.Module):
+
+    def __init__(self, ts_input_dim, static_input_dim, lstm_hidden_dim, output_dim, lstm_num_layers, dropout=0.1): 
+
+        super(LSTMClassifier, self).__init__()
+
+        self.ts_input_dim = ts_input_dim
+        self.static_input_dim = static_input_dim
+        self.lstm_hidden_dim = lstm_hidden_dim
+        self.output_dim = output_dim
+        self.lstm_num_layers = lstm_num_layers
+        self.dropout = dropout
+
+        # LSTM block to process some time series input
+        self.lstm = nn.LSTM(input_size=ts_input_dim, hidden_size=lstm_hidden_dim, num_layers=lstm_num_layers, batch_first=True)
+        
+        # Fully connected layer(s) for merging LSTM output with static features
+        self.fc1 = nn.Linear(lstm_hidden_dim + static_input_dim, 128)
+
+        # Layer to output the class probabilities.
+        self.fc_final = nn.Linear(128, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x_ts, x_static):
+        
+        # Initialize hidden and cell states with zeros
+        h0 = torch.zeros(self.lstm_num_layers, self.lstm_hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.lstm_num_layers, self.lstm_hidden_dim).requires_grad_()
+
+        # Pass the time series data through the LSTM
+        lstm_out, (hn, cn) = self.lstm(x_ts, (h0.detach(), c0.detach()))
+
+        # Concatenate LSTM output with the static features and 
+        concat_tensor = torch.concat((lstm_out[-1, :], x_static))
+        print(concat_tensor.size())
+
+        # Pass the concatenated tensors through the fully connected layers
+        out = self.fc1(concat_tensor) 
+        out = self.relu(out)
+        out = self.fc_final(out)
+        return out
+    
+if __name__=='__main__':
+
+    learning_rate = 0.001
+    ts_input_dim = 5
+    static_input_dim = 5
+    lstm_hidden_dim = 64
+    output_dim = 5
+    lstm_num_layers = 4
+
+    model = LSTMClassifier(ts_input_dim, static_input_dim, lstm_hidden_dim, output_dim, lstm_num_layers)
+
+    ts_length = 5
+    input_ts = torch.randn(ts_length, ts_input_dim)
+    input_static = torch.randn(static_input_dim)
+
+    outputs = model(input_ts, input_static)
+
+    print(outputs)
+
