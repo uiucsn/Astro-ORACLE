3a1d773b4ab290285d4b80acf696f202001ac93f
ved <vedshah.email@gmail.com>
2024-02-28 16:59:36 -0600

    Completed loop for converting parquet rows to astropy tables.

--- parquet_to_LSST_Source.py
+++ parquet_to_LSST_Source.py
@@ -1,5 +1,7 @@
 import polars as pl
+
 from pathlib import Path
+from astropy.io import ascii
 
 from LSST_Source import LSST_Source
 from argparse import ArgumentParser
@@ -10,9 +12,9 @@ def parse_args(argv=None):
         description='Convert parquet files based on SNANA FITS to LSST_Source object tensors',
     )
     parser.add_argument('input', type=Path, 
-                        help='Path to folder containing parquet files')
-    # parser.add_argument('output', type=Path,
-    #                     help='Output tensors from the feature extraction of LSST_Source objects')
+                        help='Path to the parquet file')
+    parser.add_argument('output', type=Path,
+                        help='Output tensors from the feature extraction of LSST_Source objects')
 
     return parser.parse_args(argv)
 
@@ -21,16 +23,25 @@ def main(argv=None):
     args = parse_args(argv)
 
     parquet_file_path = args.input
+    table_file_path = args.output
     object_class = str(parquet_file_path).split('.')[0].split('/')[1]
 
     df = pl.read_parquet(parquet_file_path)
-    print(f'Starting data augmentation for {parquet_file_path} Number of objects: {df.shape[0]} Feature length: {df.shape[1]}')
+    print(f'Starting data augmentation for {parquet_file_path} Number of objects: {df.shape[0]} SNANA Feature length: {df.shape[1]}')
 
     for i in range(df.shape[0]):
 
         row = df[i]
-        temp = LSST_Source(row, object_class)
-        l = temp.get_augmented_sources()
+        source = LSST_Source(row, object_class)
+        source.plot_flux_curve()
+        t  = source.get_event_table()
+
+        # Make directory
+        table_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Save the event table
+        file_name = str(table_file_path) + f'/{source.SNID}_{source.ELASTICC_class}.ecsv'
+        t.write(file_name, overwrite = False) 
 
     print(f"Completed conversion: {parquet_file_path}")
         
