342346b923e82a5853141d36471c35140f21a0ff
ved <vedshah.email@gmail.com>
2024-03-04 00:38:24 -0600

    Fixed taxnonomy plotting code

--- taxonomy.py
+++ taxonomy.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+
 # Graph to store taxonomy
-tree = nx.Graph()
+tree = nx.DiGraph(directed=True)
 
 tree.add_node('Alert', color='red')
 
@@ -22,12 +24,12 @@ tree.add_nodes_from(level_2b_nodes)
 tree.add_edges_from([('Stellar', level_2b_node) for level_2b_node in level_2b_nodes])
 
 # Level 3a nodes for SN Transients
-level_3a_nodes = ['SNa', 'SNIb/c', 'SNIax', 'SNI91bg', 'SNII']
+level_3a_nodes = ['SNIa', 'SNIb/c', 'SNIax', 'SNI91bg', 'SNII']
 tree.add_nodes_from(level_3a_nodes)
 tree.add_edges_from([('SN', level_3a_node) for level_3a_node in level_3a_nodes])
 
 # Level 3b nodes for Fast events Transients
-level_3b_nodes = ['KN', 'Dwarf Novae', 'uLens']
+level_3b_nodes = ['KN', 'Dwarf Novae', 'uLens', 'M-dwarf Flare']
 tree.add_nodes_from(level_3b_nodes)
 tree.add_edges_from([('Fast', level_3b_node) for level_3b_node in level_3b_nodes])
 
@@ -59,8 +61,10 @@ def get_astrophysical_class(elasticc_class):
 if __name__=='__main__':
 
     elasticc_class = 'KN_B19'
+    # print(nx.shortest_path(tree, source='Alert', target=get_astrophysical_class(elasticc_class)))
 
-    print(nx.shortest_path(tree, source='Alert', target=get_astrophysical_class(elasticc_class)))
+    pos = graphviz_layout(tree, prog='dot')
+    nx.draw_networkx(tree, with_labels=True, font_weight='bold', arrows=True, node_color='white', font_size = 8, pos=pos)
 
-    nx.draw_networkx(tree, with_labels=True, font_weight='bold', arrows=True, node_color='white')
+    plt.title('Taxonomy for hierarchical classification.')
     plt.show()
