765531e5034c7ac54e5642a4e2d20f1d309c7429
ved <vedshah.email@gmail.com>
2024-03-04 22:54:21 -0600

    Added code to get labels for each object

--- taxonomy.py
+++ taxonomy.py
@@ -1,7 +1,10 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 
 from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+
+source_node_label = 'Alert'
 
 def get_taxonomy_tree():
 
@@ -21,7 +24,7 @@ def get_taxonomy_tree():
     tree.add_edges_from([('Transient', level_2a_node) for level_2a_node in level_2a_nodes])
 
     # Level 2b nodes for Transients
-    level_2b_nodes = ['Periodic', 'Non-Periodic']
+    level_2b_nodes = ['Periodic', 'AGN']
     tree.add_nodes_from(level_2b_nodes)
     tree.add_edges_from([('Variable', level_2b_node) for level_2b_node in level_2b_nodes])
 
@@ -41,14 +44,9 @@ def get_taxonomy_tree():
     tree.add_edges_from([('Long', level_3c_node) for level_3c_node in level_3c_nodes])
 
     # Level 3d nodes for periodic stellar events
-    level_3d_nodes = ['Cepheid', 'RR Lyrae', 'Delta Scuti', 'EB', 'LPV/Mira']
+    level_3d_nodes = ['Cepheid', 'RR Lyrae', 'Delta Scuti', 'EB'] 
     tree.add_nodes_from(level_3d_nodes)
     tree.add_edges_from([('Periodic', level_3d_node) for level_3d_node in level_3d_nodes])
-
-    # Level 3e nodes for non periodic stellar events
-    level_3e_nodes = ['AGN',]
-    tree.add_nodes_from(level_3e_nodes)
-    tree.add_edges_from([('Non-Periodic', level_3e_node) for level_3e_node in level_3e_nodes])
 
     return tree
 
@@ -89,14 +87,40 @@ def get_astrophysical_class(elasticc_class):
                     'SNIb+HostXT_V19': 'SNIb/c', 
                     'uLens-Single_PyLIMA': 'uLens'
                 }
+    
+    tree = get_taxonomy_tree()
+    leaf_nodes = [x for x in tree.nodes() if tree.out_degree(x)==0 and tree.in_degree(x)==1]
+    assert sorted(leaf_nodes) == sorted(list(set(class_map.values())))
 
     return class_map[elasticc_class]
+
+def get_classification_labels(astrophysical_class):
+
+    tree = get_taxonomy_tree()
+
+    # Do a level order traversal of the tree to get an ordering of the nodes
+    level_order_nodes = nx.bfs_tree(tree, source=source_node_label).nodes()
+
+    # Find the path from alert to the astrophysical_class
+    path = nx.shortest_path(tree, source=source_node_label, target=astrophysical_class)
+
+    labels = np.zeros(len(level_order_nodes))
+    for node in path:
+        idx = list(level_order_nodes).index(node)
+        labels[idx] = 1
+    
+    return level_order_nodes, labels
+
 
 if __name__=='__main__':
 
     tree = get_taxonomy_tree()
-    # elasticc_class = 'KN_B19'
-    # print(nx.shortest_path(tree, source='Alert', target=get_astrophysical_class(elasticc_class)))
+
+    elasticc_class = 'KN_B19'
+    astrophysical_class = get_astrophysical_class(elasticc_class)
+    nx.shortest_path(tree, source='Alert', target=astrophysical_class)
+
+    print(get_classification_labels(astrophysical_class))
 
     pos = graphviz_layout(tree, prog='dot')
     nx.draw_networkx(tree, with_labels=True, font_weight='bold', arrows=True, node_color='white', font_size = 8, pos=pos)
