6b6304963c38cd28540869b22ed832e211307276
ved <vedshah.email@gmail.com>
2024-05-22 16:58:20 -0500

    Added new code to plot ROC curves

--- vizualizations.py
+++ vizualizations.py
@@ -1,8 +1,10 @@
 import imageio
+
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve
 
 
 def plot_confusion_matrix(y_true, y_pred, labels, title=None, img_file=None):
@@ -29,6 +31,27 @@ def plot_confusion_matrix(y_true, y_pred, labels, title=None, img_file=None):
 
     if img_file:
         plt.savefig(img_file)
+
+def plot_roc_curves(y_true, y_pred, labels, title=None, img_file=None):
+
+    chance = np.arange(0,1,0.01)
+    plt.plot(chance, chance, '--', label='Random Chance')
+
+    for i, label in enumerate(labels):
+
+        fpr, tpr, _ = roc_curve(y_true[:, i], y_pred[:, i])
+        plt.plot(fpr, tpr, label=label)
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+    #plt.legend()
+    plt.title(title)
+    plt.tight_layout()
+
+    if img_file:
+        plt.savefig(img_file)
+
 
 def make_gif(files, gif_file=None):
 
