772aa1cc2262833739b97d685723d9a7dc5b721b
ved <vedshah.email@gmail.com>
2024-05-23 23:28:44 -0500

    Improved some vizualization tools

--- vizualizations.py
+++ vizualizations.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve, roc_auc_score
 
 
 def plot_confusion_matrix(y_true, y_pred, labels, title=None, img_file=None):
@@ -35,17 +35,18 @@ def plot_confusion_matrix(y_true, y_pred, labels, title=None, img_file=None):
 def plot_roc_curves(y_true, y_pred, labels, title=None, img_file=None):
 
     chance = np.arange(0,1,0.01)
-    plt.plot(chance, chance, '--', label='Random Chance')
+    plt.plot(chance, chance, '--', color='black', label='Random Chance (AUC = 0.5)')
 
     for i, label in enumerate(labels):
 
+        score = roc_auc_score(y_true[:, i], y_pred[:, i])
         fpr, tpr, _ = roc_curve(y_true[:, i], y_pred[:, i])
-        plt.plot(fpr, tpr, label=label)
+        plt.plot(fpr, tpr, label=f"{label} (AUC = {score:.2f})")
 
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
 
-    #plt.legend()
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.3), fancybox=True, shadow=False, ncol=3, fontsize = 8)
     plt.title(title)
     plt.tight_layout()
 
